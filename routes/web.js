0c345babbc8c1a4d727fbd37daa725004cfd24ad
vishalprabhu2018 <prabhuvishal2018@gmail.com>
2023-11-29 16:22:04 +0530

    made home page dynamic with the help of controllers and database and ejs

--- routes/web.js
+++ routes/web.js
@@ -1,21 +1,16 @@
- function initRoutes(app){
+const homeController=require('../app/http/controllers/homeController');
+const authController=require('../app/http/controllers/authController');
+const cartController=require('../app/http/controllers/customers/cartController');
+
+function initRoutes(app){
     
-
-app.get('/',(req,res)=>{
-    res.render('home')
-})
-
-app.get('/cart',(req,res)=>{
-    res.render('customers/cart')
-})
-
-app.get('/login',(req,res)=>{
-    res.render('auth/login')
-})
-
-app.get('/register',(req,res)=>{
-    res.render('auth/register')
-})
+    app.get('/',homeController().index)
+    
+    app.get('/cart',cartController().index)
+    
+    app.get('/login',authController().login)
+    
+    app.get('/register',authController().register)
 
 
  }
