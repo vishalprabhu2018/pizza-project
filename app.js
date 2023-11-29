dbd8c19ff06523f0fc9f939dff824e0d463975d7
vishalprabhu2018 <prabhuvishal2018@gmail.com>
2023-11-29 10:05:38 +0530

    putting all routes from app folder to routes/web.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const ejs=require('ejs');
 const expressLayout=require('express-ejs-layouts')
 const path=require('path');
 
+require('./routes/web')(app)          //called the function with parameter 
+
 // const initialPath=path.join(__dirname,'./views')
 
 // app.use(express.static(initialPath))
@@ -19,21 +21,4 @@ app.set('view engine','ejs');
 app.use(express.static('public'));
 
 
-
-
-app.get('/',(req,res)=>{
-    res.render('home')
-})
-
-app.get('/cart',(req,res)=>{
-    res.render('customers/cart')
-})
-
-app.get('/login',(req,res)=>{
-    res.render('auth/login')
-})
-
-app.get('/register',(req,res)=>{
-    res.render('auth/register')
-})
 module.exports = app;
