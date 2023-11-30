31785f87bc5e792232b6c4bde9830892ad39778b
vishalprabhu2018 <prabhuvishal2018@gmail.com>
2023-11-30 12:55:18 +0530

    added notify fuctionality while adding pizzas in the cart with the help of noty . Also removed outline from the button

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,25 @@
-import axios from 'axios'
-
+import axios from 'axios';
+import Noty from 'noty'
 const addToCart = document.querySelectorAll('.add-to-cart');
 const cartCounter=document.getElementById('cartCounter');
 
 function updateCart(pizza){
      axios.post('/update-cart',pizza).then(res=>{
-        console.log(res)
         cartCounter.innerText=res.data.totalQty;
+        new Noty({
+            type: 'success',
+            timeout: 1000,
+            text: 'Item added to cart',
+            progressBar: false,
+            // layout:'bottomLeft'
+        }).show();
+    }).catch(err => {
+        new Noty({
+            type: 'error',
+            timeout: 1000,
+            text: 'Something went wrong',
+            progressBar: false,
+        }).show();
      }).catch((error)=>{console.log(error)});
 }
 
