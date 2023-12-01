59d3a95fb47f70924ef33b76ef8b7772c884f335
vishalprabhu2018 <prabhuvishal2018@gmail.com>
2023-12-01 13:54:40 +0530

    registration backend done

--- app/http/controllers/authController.js
+++ app/http/controllers/authController.js
@@ -1,4 +1,5 @@
 const User=require('../../models/user')
+const bcrypt=require('bcrypt');
 
 function authController(){
     return{
@@ -8,14 +9,45 @@ function authController(){
         register(req,res){
          res.render('auth/register')
         },
-        postRegister(req,res){
+         async postRegister(req,res){
          const {name, email, password}=req.body
           
           if(!name,!email,!password){
             req.flash('error','All fields are required'); 
+            req.flash('name',name);
+            req.flash('email',email);
             return res.redirect('/register')
           }
-         console.log(req.body);
+         
+          //check if email exist
+          const existingUser = await User.findOne({email})
+          if (existingUser) {
+            req.flash('error','Email already exist'); 
+            req.flash('name',name);
+            req.flash('email',email);
+            return res.redirect('/register')
+          }
+          //hash password
+    const hashedPassword= await bcrypt.hash(password,10);
+          
+          //create new user
+          const user=new User({
+            name,
+            email,
+            password:hashedPassword
+          })
+
+          user.save().then((user)=>{
+          // Login functionality add later here
+          return res.redirect('/')
+          }).catch(err=>{
+            req.flash('error','Something went wrong'); 
+            return res.redirect('/register')
+          })
+
+
+
+          console.log(req.body)
         }
     }
 }
